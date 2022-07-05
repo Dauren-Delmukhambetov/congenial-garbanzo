7a1ee948150e1c3ddc0723452938e3d8237ad545
Dauren Delmukhambetov <dauren.del@gmail.com>
2022-07-05 12:59:08 +0300

    Integrating Amazon SQS to handler to send messages (#7)

--- src/main/java/org/serverless/oqu/kerek/BookHandler.java
+++ src/main/java/org/serverless/oqu/kerek/BookHandler.java
@@ -1,11 +1,14 @@
 package org.serverless.oqu.kerek;
 
+import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
+import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.serverless.template.ApiGatewayEventHandler;
 import org.serverless.template.ClientException;
 
 import java.util.List;
+import java.util.UUID;
 
 import static java.util.stream.Collectors.toList;
 import static org.serverless.oqu.kerek.HtmlParseUtils.parseBookPagesPath;
@@ -21,7 +24,22 @@ public class BookHandler extends ApiGatewayEventHandler<BookHandler.BookParsingR
 
         if (pages.isEmpty()) throw new ClientException(404, "Pages URLs have not been found on the given URL");
 
+        sendMessagesToSqs(pages);
+
         return new BookParsingResponse(pages.stream().limit(10).collect(toList()));
+    }
+
+    private void sendMessagesToSqs(final List<String> pages) {
+        initSqsClient();
+        final var queueUrl = System.getenv("QUEUE_NAME");
+        final var entries = pages.stream()
+                .limit(25)
+                .map(pageUrl -> new SendMessageBatchRequestEntry(UUID.randomUUID().toString(), pageUrl))
+                .collect(toList());
+        SendMessageBatchRequest sqsBatchRequest = new SendMessageBatchRequest()
+                .withQueueUrl(queueUrl)
+                .withEntries(entries);
+        sqs.sendMessageBatch(sqsBatchRequest);
     }
 
     @Getter
