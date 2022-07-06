ea54db1bed0240d8fb98a76a69f08ed9669866ad
Dauren-Delmukhambetov <dauren.del@gmail.com>
2022-07-06 17:32:15 +0300

    Fixing message attributes retrieval

--- src/main/java/org/serverless/oqu/kerek/BookPageLoader.java
+++ src/main/java/org/serverless/oqu/kerek/BookPageLoader.java
@@ -21,7 +21,7 @@ public class BookPageLoader extends SqsEventHandler {
            final var bucketName = System.getenv("BUCKET_NAME");
            final var pageUrl = new URL("https://kazneb.kz" + input.getBody().replace("&amp;", "&"));
            log(context, "Page URL for downloading is %s", pageUrl.getPath());
-           final var bookId = input.getAttributes().getOrDefault("book-id", "unknown");
+           final var bookId = input.getMessageAttributes().containsKey("book-id") ? input.getMessageAttributes().get("book-id").getStringValue() : "unknown";
            final var filepath = format("%s/%s", bookId, getName(pageUrl.getPath()));
            final var metadata = new ObjectMetadata();
            metadata.setContentType("image/" + getExtension(pageUrl.getPath()));
