c9441fb3c782ee74c04e60a71955de6bd0f4571d
Dauren Delmukhambetov <dauren.del@gmail.com>
2022-07-08 15:58:54 +0300

    #18: Introducing async book page URLs fetching (#19)
    * #18: Introducing async book page URLs fetching
    
    * #18: Removing obsolete test

---
+++ src/main/java/org/serverless/oqu/kerek/BookPagesURLFetcher.java
@@ -0,0 +1,111 @@
+package org.serverless.oqu.kerek;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.sqs.model.MessageAttributeValue;
+import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
+import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
+import org.serverless.template.ClientException;
+import org.serverless.template.SqsEventHandler;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.amazonaws.util.StringUtils.isNullOrEmpty;
+import static java.lang.String.format;
+import static java.util.Map.Entry.comparingByKey;
+import static java.util.Objects.requireNonNull;
+import static java.util.UUID.randomUUID;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+import static org.apache.commons.io.FilenameUtils.getExtension;
+import static org.apache.commons.io.FilenameUtils.getName;
+import static org.apache.commons.io.FilenameUtils.removeExtension;
+import static org.serverless.oqu.kerek.HtmlParseUtils.parseBookPagesUrls;
+import static org.serverless.oqu.kerek.URLUtils.extractQueryParamValue;
+
+public class BookPagesURLFetcher extends SqsEventHandler {
+
+    @Override
+    protected Void doHandleRequest(SQSEvent.SQSMessage input, Context context) {
+        try {
+            log(context, "Starting processing SQS message (ID = %s)", input.getMessageId());
+            final var bookId = extractQueryParamValue(input.getBody(), "brId");
+            final var pages = parseBookPagesUrls(input.getBody());
+
+            if (pages.isEmpty()) throw new ClientException(404, "Pages URLs have not been found on the given URL");
+
+            sendMessagesToSqs(pages, bookId);
+            log(context, "Completed processing SQS message (ID = %s)", input.getMessageId());
+        } catch (Exception e) {
+            log(context, "Error occurred while processing request SQS message (ID = %s): %s", input.getMessageId(), e.getMessage());
+        }
+        return null;
+    }
+
+    private void sendMessagesToSqs(final List<String> pages, final String bookId) {
+        initSqsClient();
+        final var queueUrl = System.getenv("QUEUE_NAME");
+        final var pagesWithoutLastOne = pages.subList(0, pages.size() - 1);
+        final var lastPage = pages.get(pages.size() - 1);
+        final var chuckedPages = chuckList(pagesWithoutLastOne, SQS_BATCH_REQUEST_LIMIT);
+
+        for (final var urls : chuckedPages) {
+            final var entries = urls.stream()
+                    .map(pageUrl -> buildSendMessageRequest(pageUrl, bookId, null))
+                    .filter(Objects::nonNull)
+                    .collect(toList());
+            final var sqsBatchRequest = new SendMessageBatchRequest()
+                    .withQueueUrl(queueUrl)
+                    .withEntries(entries);
+            sqs.sendMessageBatch(sqsBatchRequest);
+        }
+
+        sqs.sendMessageBatch(
+                new SendMessageBatchRequest()
+                        .withQueueUrl(queueUrl)
+                        .withEntries(List.of(requireNonNull(buildSendMessageRequest(lastPage, bookId, "last"))))
+        );
+    }
+
+    private SendMessageBatchRequestEntry buildSendMessageRequest(final String pageUrl, final String bookId, final String filename) {
+        try {
+            final var url = new URL("https://kazneb.kz" + pageUrl.replace("&amp;", "&"));
+            final var filenameWithoutExtension = isNullOrEmpty(filename) ? removeExtension(getName(url.getPath())) : filename;
+            final var extension = getExtension(url.getPath());
+            final var attributes = Map.of(
+                    "filepath", format("%s/%s.%s", bookId, filenameWithoutExtension, extension),
+                    "content-type", format("image/%s", extension)
+            );
+            final var messageAttributes = attributes.entrySet()
+                    .stream()
+                    .collect(toMap(
+                            Map.Entry::getKey,
+                            e -> new MessageAttributeValue()
+                                    .withDataType(String.class.getSimpleName())
+                                    .withStringValue(e.getValue())
+                    ));
+            return new SendMessageBatchRequestEntry(randomUUID().toString(), url.toString())
+                    .withMessageAttributes(messageAttributes);
+        } catch (MalformedURLException e) {
+            System.err.printf("Error occurred while trying to parse URL %s : %s%n", pageUrl, e.getMessage());
+        }
+        return null;
+    }
+
+    private List<List<String>> chuckList(final List<String> pages, final Integer chunkSize) {
+        AtomicInteger index = new AtomicInteger(0);
+        return pages.stream().collect(groupingBy(x -> index.getAndIncrement() / chunkSize))
+                .entrySet().stream()
+                .sorted(comparingByKey())
+                .map(Map.Entry::getValue)
+                .collect(toList());
+    }
+
+    private static final Integer SQS_BATCH_REQUEST_LIMIT = 10;
+}
---
+++ src/test/java/org/serverless/oqu/kerek/HtmlParseUtilsTest.java
@@ -0,0 +1,31 @@
+package org.serverless.oqu.kerek;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.serverless.oqu.kerek.HtmlParseUtils.parseBookInfo;
+
+class HtmlParseUtilsTest {
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "1628186|Шәмші Қалдаяқов - ән патшасы|Әкімқұлов, Е.",
+                    "82809|Айқап|n/a",
+                    "82915|Ақ кеме. Бетпе-бет. Алғашқы ұстаз|Айтматов, Ш.",
+                    "1093883|Каталог книгопродавца двора Его Императорского Величества А. Смирдина (сына) и К|n/a"
+            },
+            delimiter = '|',
+            nullValues = {"n/a"}
+    )
+    void shouldParseBookInfo(final String bookId, final String title, final String author) {
+        final var bookInfo = parseBookInfo(format("https://kazneb.kz/ru/catalogue/view/%s", bookId));
+
+        assertNotNull(bookInfo);
+        assertNotNull(bookInfo.getImageUrl());
+        assertEquals(title, bookInfo.getTitle());
+        assertEquals(author, bookInfo.getAuthor());
+    }
+}
