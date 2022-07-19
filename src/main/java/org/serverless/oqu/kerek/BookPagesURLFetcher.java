cbdb315df87657fcd7b8aa1b53d30e1196c0fb04
Dauren Delmukhambetov <dauren.del@gmail.com>
2022-07-19 11:16:57 +0300

    Fixing FIFO queue by adding message group ID (#32)

--- src/main/java/org/serverless/oqu/kerek/BookPagesURLFetcher.java
+++ src/main/java/org/serverless/oqu/kerek/BookPagesURLFetcher.java
@@ -102,6 +102,7 @@ public class BookPagesURLFetcher extends SqsEventHandler {
                     ));
             return SendMessageBatchRequestEntry.builder()
                     .id(randomUUID().toString())
+                    .messageGroupId(bookId)
                     .messageBody(url.toString())
                     .messageAttributes(messageAttributes)
                     .build();
