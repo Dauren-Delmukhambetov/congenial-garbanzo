87932290ab5e24897937954a794b0a52b1a18381
Dauren-Delmukhambetov <dauren.del@gmail.com>
2022-07-29 16:52:59 +0300

    Refactoring email sending client

--- src/main/java/org/serverless/oqu/kerek/BookReadinessNotifier.java
+++ src/main/java/org/serverless/oqu/kerek/BookReadinessNotifier.java
@@ -4,21 +4,19 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
 import org.serverless.template.S3EventHandler;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.services.ses.model.Body;
-import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 
 import java.net.URL;
 import java.time.Duration;
 
 import static java.lang.String.format;
 import static org.serverless.oqu.kerek.Constants.S3_OBJECT_INITIATOR_EMAIL_ATTR;
+import static org.serverless.oqu.kerek.EmailUtils.sendEmail;
 
 public class BookReadinessNotifier extends S3EventHandler {
 
     static {
         initS3Client();
         initS3Presigner();
-        initSesClient();
     }
 
     @Override
@@ -30,30 +28,13 @@ public class BookReadinessNotifier extends S3EventHandler {
             final var directory = input.getS3().getObject().getKey().split("/")[0];
             final var email = acquireInitiatorEmail(bucketName, directory);
             final var url = buildPresignedUrlToPdfFile(bucketName, directory);
-            sendEmail(url, email);
+            sendEmail(email, url);
 
             log(context, "Completed processing S3 Event notification record (Object Key = %s)", input.getS3().getObject().getKey());
         } catch (Exception e) {
             log(context, "Error occurred while processing S3 Event notification record (Object Key = %s): %s", input.getS3().getObject().getKey(), e.getMessage());
         }
         return null;
-    }
-
-    private void sendEmail(final URL linkToFile, final String email) {
-
-        final var content = Body.builder()
-                .html(c -> c.data("<html> <head></head> <body> <a href=\"" + linkToFile.toExternalForm() + "\">Link to file</a> </body></html>"))
-                .build();
-
-        SendEmailRequest emailRequest = SendEmailRequest.builder()
-                .destination(d -> d.toAddresses(email))
-                .message(m -> m.subject(s -> s.data("Your book is ready to download")).body(content))
-                .source("dauren_del@mail.ru")
-                .build();
-
-        final var response = sesClient.sendEmail(emailRequest);
-
-        System.out.printf("Email with ID (%s) has been send to user", response.messageId());
     }
 
     private URL buildPresignedUrlToPdfFile(final String bucketName, final String directory) {
---
+++ src/main/java/org/serverless/oqu/kerek/EmailUtils.java
@@ -0,0 +1,55 @@
+package org.serverless.oqu.kerek;
+
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.net.URL;
+import java.util.Properties;
+
+public final class EmailUtils {
+
+    private EmailUtils() {}
+
+    public static void sendEmail(final String to, final URL linkToFile) {
+        final var from = System.getenv("SENDER_EMAIL") + "@gmail.com";
+        final var password = System.getenv("SENDER_PASSWORD");
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new jakarta.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        session.setDebug(true);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Your book is ready");
+            message.setText("<html> <head></head> <body> <a href=\"" + linkToFile.toExternalForm() + "\">Link to file</a> </body></html>");
+
+            System.out.println("sending...");
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
