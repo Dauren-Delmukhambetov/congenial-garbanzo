1c50c1969ef8584d3dd3c046446f7436844c9d4b
Dauren-Delmukhambetov <dauren.del@gmail.com>
2022-09-13 17:51:30 +0300

    #25: Fixing tests

--- src/main/java/org/serverless/template/TemplateHandler.java
+++ src/main/java/org/serverless/template/TemplateHandler.java
@@ -1,6 +1,7 @@
 package org.serverless.template;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +13,11 @@ public class TemplateHandler extends ApiGatewayEventHandler<TemplateHandler.Requ
     protected Response doHandleRequest(final Request input, final Context context) {
         log(context, "Template handler method has been invoked");
         return new Response(input.data);
+    }
+
+    @Override
+    protected Request getRequestData(APIGatewayProxyRequestEvent input) {
+        return gson.fromJson(input.getBody(), TemplateHandler.Request.class);
     }
 
     @Getter
