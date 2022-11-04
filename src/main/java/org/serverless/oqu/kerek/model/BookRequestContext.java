cad6efba343b182bb8eb80f5276ed58ab2ffdbf1
Dauren Delmukhambetov <dauren.del@gmail.com>
2022-11-04 17:39:11 +0300

    #42: Adding persistence layer in DynamoDb (#49)

---
+++ src/main/java/org/serverless/oqu/kerek/model/BookRequestContext.java
@@ -0,0 +1,15 @@
+package org.serverless.oqu.kerek.model;
+
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+import java.time.OffsetDateTime;
+
+@Builder
+@Value
+@RequiredArgsConstructor
+public class BookRequestContext {
+    String userEmail;
+    OffsetDateTime requestedAt;
+}
